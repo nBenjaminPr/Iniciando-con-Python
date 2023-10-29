40df7abb4a7f7bf00dc13c922abc1bf20fd82655
Nicolas <nicobenpe@gmail.com>
2023-10-29 20:51:58 -0300

    usandos Types

--- tarea.py
+++ tarea.py
@@ -1,1 +1,15 @@
+# Es un comentario: Hola Mundo
 print ("Hola mundo")
+
+"""
+Es un comentario
+"""
+
+#Consultar el tipo de dato
+
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
