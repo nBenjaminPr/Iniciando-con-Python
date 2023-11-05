94550473be0b069c3a76388a0153310f71b518ba
Nicolas <nicobenpe@gmail.com>
2023-11-05 20:27:13 -0300

    concatenaciòn de var.

--- 01_variables.py
+++ 01_variables.py
@@ -1,4 +1,19 @@
 # Variables
 
-MyVariable = "My String Variable"
-print(MyVariable)
+my_variable = "My String Variable"
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+
+my_bool_variable = False
+print(my_bool_variable)
+
+#Concatenacion de variables en un print
+
+print (type(print(my_variable, my_int_to_str_variable,my_bool_variable)))
