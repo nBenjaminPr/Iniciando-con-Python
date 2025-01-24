60ec7ca353028298ea87433303b3d34f41405845
Nicolas <nicobenpe@gmail.com>
2025-01-24 15:37:08 -0300

    Code Python

---
+++ Hola mundo.py
@@ -0,0 +1,4 @@
+nombre = input("Introducir nombre: ")
+edad = input("Introducir edad: ")  # Comentario
+
+print(f"Hola, {nombre} y tengo {edad} años")
---
+++ Variables.py
@@ -0,0 +1,26 @@
+a = "Palabra"
+b= "5"
+c= 8
+d= 4.5
+e= True
+
+print(type(a))  # Esto imprimirá: <class 'str'>
+print(type(b))  # Esto imprimirá: <class 'str'>
+print(type(c))  # Esto imprimirá: <class 'int'>
+print(type(d))  # Esto imprimirá: <class 'float'>
+print(type(e))  # Esto imprimirá: <class 'bool'>
+
+print (a + a)
+
+
+#LISTAS
+lista = ["argentina", 15,8.5,"Rose"]
+
+print (lista [2])
+
+
+#DICCIONARIO
+diccionario = {"Pais": "Argentina", "Año indpendecia": 1812, "capital": "Bs As", "Provincias": 24}
+
+print (diccionario ["Pais"])
+print (diccionario ["Provincias"])
