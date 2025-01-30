2fcd381fc5c350c9a7393cdea9bdae6f3315b3d1
Nicolas <nicobenpe@gmail.com>
2025-01-30 04:43:57 -0300

    tuplas y set

--- Estructura de datos/EstrucDicc.py
+++ Estructura de datos/EstrucDicc.py
@@ -16,3 +16,15 @@ edades = list(diccionario_edades.values())
 
 print (nombres)
 print (edades)
+
+#Tuplas son parecidas a las listas pero son inmutables
+
+mi_tupla= (1,2,3,4)
+print (mi_tupla[3])
+#Tuplas son inmutables, no se pueden modificar
+
+
+#Sets: es util para eliminar elementos duplicados de una lista
+
+my_set = set([1,2,3,4,4,4])
+print(my_set)
